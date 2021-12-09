3d294746d93b9fab60b9e2d254279d1b9b3d2ac7
Maisha Ahmed <58888586+maisha-ahmed@users.noreply.github.com>
2021-12-08 19:10:50 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,56 +1,56 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source  = "hashicorp/aws"
-#       version = "3.26.0"
-#     }
-#     random = {
-#       source  = "hashicorp/random"
-#       version = "3.0.1"
-#     }
-#   }
-#   required_version = ">= 0.14"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 0.14"
 
-#   backend "remote" {
-#     organization = "example-org-be0c42"
+  backend "remote" {
+    organization = "example-org-be0c42"
 
-#     workspaces {
-#       name = "gh-actions-demo"
-#     }
-#   }
-# }
-
-
-# provider "aws" {
-#   region = "us-west-2"
-# }
+    workspaces {
+      name = "gh-actions-demo"
+    }
+  }
+}
 
 
+provider "aws" {
+  region = "us-west-2"
+}
 
-# resource "random_pet" "sg" {}
 
-# resource "aws_instance" "web" {
-#   ami                    = "ami-830c94e3"
-#   instance_type          = "t2.micro"
-#   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
-#   user_data = <<-EOF
-#               #!/bin/bash
-#               echo "Hello, World" > index.html
-#               nohup busybox httpd -f -p 8080 &
-#               EOF
-# }
+resource "random_pet" "sg" {}
 
-# resource "aws_security_group" "web-sg" {
-#   name = "${random_pet.sg.id}-sg"
-#   ingress {
-#     from_port   = 8080
-#     to_port     = 8080
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-# }
+resource "aws_instance" "web" {
+  ami                    = "ami-830c94e3"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
 
-# output "web-address" {
-#   value = "${aws_instance.web.public_dns}:8080"
-# }
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+}
+
+resource "aws_security_group" "web-sg" {
+  name = "${random_pet.sg.id}-sg"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "web-address" {
+  value = "${aws_instance.web.public_dns}:8080"
+}
